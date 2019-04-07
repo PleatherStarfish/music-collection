ee12b2338c84849baaccd0d472bfa0634228e2e5
Daniel Miller <lontano.composer@gmail.com>
2019-04-07 15:46:22 -0400

    added switch statement for user input and moved all important data and methods into an ES6 class

--- cli.js
+++ cli.js
@@ -3,7 +3,37 @@
 const inquirer = require("inquirer");
 const readline = require('readline');
 
-const musicCollection = {};        // Object to store music collection
+class MusicCollection {            // Class to store music collection and methods for collection
+    constructor() {
+        this.collection = new Map();
+    }
+
+    add(album, artist, played=false) {
+
+        this.collection.set(album, {["title"]: album, ["artist"]: artist, ["played"]: played });
+        console.log(`Added ${album} by ${artist}`);
+        console.log(this.collection);
+    }
+
+    play(album) {
+
+    }
+
+    get showAll() {
+
+    }
+
+    get getPlayed() {
+
+    }
+
+    get getUnplayed() {
+
+    }
+};
+
+const musicCollection = new MusicCollection();
+
 const parseInput = /\w+|"[^"]*"/g; // Split input on words and quotes
 
 const rl = readline.createInterface({
@@ -11,28 +41,46 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+console.log("Welcome to your music collection! \n")
 const getUserInput = function() {
-    rl.question("Welcome to your music collection! \n", function(answer) {
+    rl.question("> ", function(answer) {
 
-        const input = answer.match(parseInput);
+        const input = answer.match(parseInput); // Split input into a list of statement
 
-        if (answer === "quit") {
-            console.log("Bye!");
-            rl.close();
-        }
-        else if (input[0] === "add") {
+        switch(input[0]) {
 
-            const album = input[1];
-            const artist = input[2].slice(1, -1); // Store artist without quotes.
+            case("quit"):
+                console.log("Bye!");
+                rl.close();
 
-            musicCollection[album] = artist; // Add album and artist as key/value to object
+            case("add"):
 
-            console.log(`Added ${album} by ${artist}`);
+                if (input.length !== 3) {
 
-        } else {
-            getUserInput();
+                    console.log("Input must be in the form:\nadd \"album\" \"artist\"");
+                    getUserInput();
+                }
+                else {
+
+                    const album = input[1];
+                    const artist = input[2].slice(1, -1); // Store artist without quotes.
+
+                    // Check if an album is already in the collection
+                    if (musicCollection.collection.has(album)) {
+                        console.log("Album is already in the collection.");
+                        getUserInput();
+                    } else {
+                        musicCollection.add(album, artist);
+                        getUserInput();
+                    }
+                }
+
+            case("show all"):
+
+            default:
+                console.log("Unknown command. Please try again.")
+                getUserInput();
         }
     });
-};
 
 getUserInput();
