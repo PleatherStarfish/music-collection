ed0aa739516e02acbbccfd3d127a96dae72a14b3
Daniel Miller <lontano.composer@gmail.com>
2019-04-07 22:23:14 -0400

    added caching for played, unplayed, etc.

--- cli.js
+++ cli.js
@@ -3,32 +3,82 @@
 const inquirer = require("inquirer");
 const readline = require('readline');
 
-class MusicCollection {            // Class to store music collection and methods for collection
+// MusicCollection class instances represent music collections and methods on those collections
+//
+//   Note: This collection uses the album title as a unique key. This choice was made in order to fully exploit the
+//         unique key feature of the Map, which ensures that album titles will always be unique (per instructions).
+//         However, for some use cases it would be better to use a unique alphanumeric key generated from, for
+//         example, the date and time when the album was added. This would allow multiple albums with the same name
+//         to be saved as separate entries.
+//
+class MusicCollection {
+
     constructor() {
-        this.collection = new Map();
+        // Map because we must be able to return entries in FIFO order
+        this.collection         = new Map();
+
+        // We cache "played" and "unplayed" for a more performant retrieval
+        this.playedTracks       = new Map();
+        this.unplayedTracks     = new Map();
+        this.collectionByArtist = new Map();
     }
 
     add(album, artist, played=false) {
 
-        this.collection.set(album, {["title"]: album, ["artist"]: artist, ["played"]: played });
+        // Add album to main collection
+        this.collection.set(album, { ["title"]: album, ["artist"]: artist, ["played"]: played });
+
+        // Cache album as unplayed unless "played" property is true
+        if (played) {
+            this.playedTracks.set(album, {["title"]: album, ["artist"]: artist});
+        }
+        else {
+            this.unplayedTracks.set(album, {["title"]: album, ["artist"]: artist});
+        }
+
+        // Cache data in new table by artist name
+        if (this.collectionByArtist.has(artist)) {
+            this.collectionByArtist.get(artist).push({ ["title"]: album, ["artist"]: artist, ["played"]: played });
+        }
+        else {
+            this.collectionByArtist.set(artist, new Array({ ["title"]: album, ["artist"]: artist, ["played"]: played }));
+        }
+
         console.log(`Added ${album} by ${artist}`);
-        console.log(this.collection);
+        console.log(this.collectionByArtist);
     }
 
     play(album) {
+        if (this.collection.has(album)) {
+            console.log(`You're listening to ${album}`); // Inform user album is playing
+            this.collection.get(album).played = true;    // Set albums play status to 'played'
 
+            const playedArtist =  this.collection.get(album).artist;
+            this.playedTracks.set(album, { ["title"]: album, ["artist"]: playedArtist}); // cache played status
+
+            this.unplayedTracks.delete(album);
+        }
+        else {
+            console.log(`${album} is not yet in your collection.`)
+        }
     }
 
     showAll() {
-        return this.collection.forEach((value, key) => console.log(`${key} by ${value.artist}`));
+        return this.collection.forEach((value, key) =>
+            console.log(`${key} by ${value.artist} (${value.played ? "played" : "unplayed"})`)
+        );
+    };
+
+    getPlayed() {
+        return this.playedTracks.forEach((value, key) =>
+            console.log(`${key} by ${value.artist}`)
+        );
     }
 
-    get getPlayed() {
-
-    }
-
-    get getUnplayed() {
-
+    getUnplayed() {
+        return this.unplayedTracks.forEach((value, key) =>
+            console.log(`${key} by ${value.artist}`)
+        );
     }
 };
 
@@ -45,8 +95,9 @@ console.log("Welcome to your music collection! \n")
 const getUserInput = function() {
     rl.question("> ", function (answer) {
 
-        const input = answer.match(parseInput); // Split input into a list of statement
+        const input = answer.match(parseInput); // Split input into commands with RegEx
 
+        // Switch statement evaluates user input commands
         switch (input[0]) {
 
             case("quit"):
@@ -56,14 +107,14 @@ const getUserInput = function() {
 
             case("add"):
 
+                const album = input[1];
+                const artist = input[2].slice(1, -1); // Store artist without quotes.
+
                 if (input.length !== 3) {
 
                     console.log("Input must be in the form:\nadd \"album\" \"artist\"");
                     getUserInput();
                 } else {
-
-                    const album = input[1];
-                    const artist = input[2].slice(1, -1); // Store artist without quotes.
 
                     // Check if an album is already in the collection
                     if (musicCollection.collection.has(album)) {
@@ -82,12 +133,30 @@ const getUserInput = function() {
                 getUserInput();
                 break;
 
+            case("play"):
+
+                musicCollection.play(input[1]);
+                getUserInput();
+                break;
+
+            case("show played"):
+
+                musicCollection.getPlayed();
+                getUserInput();
+                break;
+
+            case("show unplayed"):
+
+                musicCollection.getUnplayed();
+                getUserInput();
+                break;
+
             default:
                 console.log("Unknown command. Please try again.");
                 getUserInput();
                 break;
         }
     });
-}
+};
 
-getUserInput();
+getUserInput(); // Call function to start interacting with a new music collection.
